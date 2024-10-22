682f38fa1055dc08f8a0e06c9221d8f59c6023b9
Pawan Singh <105308770+pawan1910@users.noreply.github.com>
2024-10-22 17:10:32 +0530

    Add files via upload

---
+++ src/app/data.js
@@ -0,0 +1,123 @@
+/*
+Websites:
+
+- https://github.com/pmndrs/gltfjsx (GLTF JSX for 3D Models)
+- https://lucide.dev/icons/ (Lucide Icons)
+- https://github.com/anuraghazra/github-readme-stats (Github Readme Stats)
+- https://skillicons.dev (Skill Icons to show skills)
+- https://github-readme-streak-stats.herokuapp.com (Github Readme Streak Stats)
+
+:root {
+  --background: 27 27 27;
+  --foreground: 225 225 225;
+  --muted: 115 115 115;
+  --accent: 254 254 91; #FEFE5B
+}
+
+*/
+
+export const projectsData = [
+  {
+    id: 1,
+    name: "Todo List",
+    description: "Add and manage your tasks",
+    date: "2024-10-15",
+    demoLink: "https://superb-axolotl-904a23.netlify.app/",
+  },
+  {
+    id: 2,
+    name: "Blog",
+    description: "Adding Blog",
+    date: "2024-06-20",
+    demoLink: "https://artgalleryonline.example.com",
+  },
+  {
+    id: 3,
+    name: "Earth",
+    description: "Globe in 3D",
+    date: "2024-09-10",
+    demoLink: "https://budgetplanner.example.com",
+  },
+  {
+    id: 4,
+    name: "HealthBeat",
+    description: "Monitor heart rate zones",
+    date: "2023-05-30",
+    demoLink: "https://healthbeat.example.com",
+  },
+  {
+    id: 5,
+    name: "RecipeFinder",
+    description: "Discover new recipes",
+    date: "2023-07-12",
+    demoLink: "https://recipefinder.example.com",
+  },
+  {
+    id: 6,
+    name: "JourneyLogger",
+    description: "Log your travels",
+    date: "2023-10-01",
+    demoLink: "https://journeylogger.example.com",
+  },
+  {
+    id: 7,
+    name: "StudyBuddy",
+    description: "Collaborative learning platform",
+    date: "2023-04-18",
+    demoLink: "https://studybuddy.example.com",
+  },
+  {
+    id: 8,
+    name: "TechTalk",
+    description: "Tech news aggregator",
+    date: "2022-11-05",
+    demoLink: "https://techtalk.example.com",
+  },
+  {
+    id: 9,
+    name: "FitTrack",
+    description: "Fitness and workout tracker",
+    date: "2022-03-22",
+    demoLink: "https://fittrack.example.com",
+  },
+  {
+    id: 10,
+    name: "MindfulMoments",
+    description: "Meditation and mindfulness app",
+    date: "2022-02-14",
+    demoLink: "https://mindfulmoments.example.com",
+  },
+];
+
+export const BtnList = [
+  { label: "Home", link: "/", icon: "home", newTab: false },
+  { label: "About", link: "/about", icon: "about", newTab: false },
+  { label: "Projects", link: "/projects", icon: "projects", newTab: false },
+  { label: "Contact", link: "/contact", icon: "contact", newTab: false },
+  {
+    label: "Github",
+    link: "https://www.github.com/pawan1910",
+    icon: "github",
+    newTab: true,
+  },
+  {
+    label: "LinkedIn",
+    link: "https://www.linkedin.com/in/pawan-singh-84a73186/",
+    icon: "linkedin",
+    newTab: true,
+  },
+  // {
+  //   label: "X",
+  //   link: "https://www.x.com/code_bucks",
+  //   icon: "twitter",
+  //   newTab: true,
+  // },
+  {
+    label: "Resume",
+    link: "/resume.pdf",
+    icon: "resume",
+    newTab: true,
+  },
+];
+
+
